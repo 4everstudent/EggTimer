7459f3a47a244b2ddd9664415446e05ea71c6cc6
4EverStudent <mail@4everstudent.com>
2016-10-01 19:53:53 +0100

    Add visuals.

--- app/src/main/java/a4everstudent/eggtimer/MainActivity.java
+++ app/src/main/java/a4everstudent/eggtimer/MainActivity.java
@@ -3,10 +3,10 @@ package a4everstudent.eggtimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-// TODO: 01-10-2016 add egg image 
-// TODO: 01-10-2016 add slider
-// TODO: 01-10-2016 add number view
-// TODO: 01-10-2016 add button Start/Stop
+
+
+
+
 // TODO: 01-10-2016 connect slider
 // TODO: 01-10-2016 connect button
 
